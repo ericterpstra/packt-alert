d9e94c5c8bf2531e8f3a0b4e0cab249686cc6783
Joe Ferguson <joe@joeferguson.me>
2015-09-09 21:17:28 -0500

    Optionally load twilio and slack libs as needed based on env Process differently if they're not enabled

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var dotenv = require('dotenv').load();
 var scheduler = require('node-schedule');
 var packtalertCommands = require('./lib/packtalert-commands');
-var packtalertMessager = require('./lib/packtalert-messager-twilio');
+// Should we load twilio stuff?
+if (process.env.USE_TWILIO == 'true') {
+    var packtalertMessager = require('./lib/packtalert-messager-twilio');
+}
+// Should we load slack stuff?
+if (process.env.USE_SLACK == 'true') {
+    var packtalertMessager = require('./lib/packtalert-messager-slack');
+}
+
 
 // Listen for a response from the user and do a callback function if response is acceptable
 packtalertMessager.awaitResponse(packtalertCommands.addToLib);
@@ -16,9 +24,17 @@ function fetchBookTitleAndSend(){
     console.log('Fecthing the title of the free book.');
 
     // Get the book of the day name and send it to the user
-    packtalertCommands.getBookOfTheDayName()
-        .then(packtalertMessager.sendBookTitle)
-        .catch(handleError);
+    if ((process.env.USE_TWILIO == 'true') || (process.env.USE_SLACK == 'true')) {
+        packtalertCommands.getBookOfTheDayName()
+            .then(packtalertMessager.sendBookTitle)
+            .then(packtalertCommands.addToLib)
+            .catch(handleError);
+    } else {
+        packtalertCommands.getBookOfTheDayName()
+            .then(packtalertCommands.addToLib)
+            .catch(handleError);
+    }
+
 }
 
 function handleError(errMsg) {
