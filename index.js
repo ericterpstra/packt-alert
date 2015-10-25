987c446afba2f195b07669b72b34cd48f90ee9f4
Joe Ferguson <joe@joeferguson.me>
2015-10-24 21:49:20 -0500

    Optionally load twilio and slack libs as needed based on env Process differently if they're not enabled

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (process.env.USE_SLACK == 'true') {
 
 
 // Listen for a response from the user and do a callback function if response is acceptable
-packtalertMessager.awaitResponse(packtalertCommands.addToLib);
+//packtalertMessager.awaitResponse(packtalertCommands.addToLib);
 
 // Schedule a task for once a day
-var schedulerCronString = process.env.CRON_SCHEDULE || '0 0 20 * * *';
-scheduler.scheduleJob(schedulerCronString, fetchBookTitleAndSend);
+//var schedulerCronString = process.env.CRON_SCHEDULE || '0 0 20 * * *';
+//scheduler.scheduleJob(schedulerCronString, fetchBookTitleAndSend);
 
-//fetchBookTitleAndSend();
+fetchBookTitleAndSend();
 
 function fetchBookTitleAndSend(){
     console.log('Fecthing the title of the free book.');
